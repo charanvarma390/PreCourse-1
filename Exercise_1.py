b0de838430ae595fe9ec56ceae6d321b1697728f
charanvarma390 <charan@CV64.>
2024-09-16 04:10:22 -0500

    Partially Done Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,33 @@
+# Time Complexity : O(1) for all operations, Except for show() which is O(n)
+# Space Complexity : O(n)
+# Any problem you faced while coding this : Trying to understand the pre-defined functions initially
+
+# Approach:       
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
+     def __init__(self): #Firstly initialize an empty stack with name self.
+      self.stack = []
          
-     def isEmpty(self):
+     def isEmpty(self) -> bool: #To check the stack is empty or not using with bool datatype to return true/false.
+      return len(self.stack) == 0
          
-     def push(self, item):
+     def push(self, item): #Using append function we are pushing a new element into the stack.
+      return self.stack.append(item)
          
-     def pop(self):
+     def pop(self): #Checking if the stack is empty or not, If not then pop the last elemnt from the stack using pop function.
+      if not self.isEmpty():
+            return self.stack.pop()
+      return None
+    
+     def peek(self): #Checking if the stack is empty or not, If not return the last(peek) element from the stack.
+      if not self.isEmpty():
+            return self.stack[-1]
+      return None
         
-        
-     def peek(self):
-        
-     def size(self):
+     def size(self): #To check the length of stack using len function.
+      return len(self.stack)
          
-     def show(self):
+     def show(self): #To display all elements in the stack.
+      return self.stack
          
 
 s = myStack()
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,8 +1,15 @@
+# Time Complexity : O(n) for all operations
+# Space Complexity : O(1)
+#Any problem you faced while coding this : Trying to understand the pre-defined functions initially
+
+#Approach: Commented the approach in the code
 class ListNode:
     """
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +24,19 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data)
+        #If the LinkedList is empty then the new node is appended as head of the list
+        if not self.head:
+            self.head = new_node
+        return
+
+        #If the LinkedList is not empty then we have to traverse till the last node 
+        current = self.head
+        while current.next:
+            current = current.next
+        
+        #Then append the new node
+        current.next = new_node
         
     def find(self, key):
         """
@@ -24,9 +44,40 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        #Assign current as pointer to head of the LinkedList
+        current = self.head
+        #Traverse through the linked list till it is empty
+        while current: 
+            #If data matches with the key in the current node then return it
+            if current.data == key: 
+                return current.data
+            #Else update the current to next node
+            current = current.next 
+        #return none if the key is not found in the LinkedList
+        return None 
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        #Assign current pointer to head and previous as none (Before head node)
+        current = self.head
+        previous = None
+
+        #If the key to be removed is the head node
+        if  current and current.data == key:
+            self.head = current.next
+        return
+
+        #If the key is not head then traverse the LinkedList
+        while current and current.data != key:
+            previous = current
+            current = current.next 
+
+        #If key is not found in any of the nodes then return None
+        if not current:
+            return None
+        
+        #If key is found then remove it by updating the previous and current pointers
+        previous.next = current.next
